ccd7d231b9bfcf9ed4d6fa26cebd7503248c1f96
Jon Ball <jrball3@verizon.net>
2019-02-04 23:48:02 -0500

    env var updates

--- topix/app.js
+++ topix/app.js
@@ -4,10 +4,9 @@ require('dotenv').config();
 const restify = require('restify');
 
 const server = restify.createServer();
+
 const hello = new V1HelloApi();
 hello.apply(server);
-
-console.log(process.env);
 
 server.listen(process.env.API_PORT, function() {
   console.log('%s listening at %s', server.name, server.url);
