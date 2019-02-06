fa9d0db27661179f22324279ffa8074a8a4b5ba8
Jon Ball <jrball3@verizon.net>
2019-02-06 01:10:01 -0500

    cleanup, remove hello stuff

--- topix/app.js
+++ topix/app.js
@@ -1,35 +1,30 @@
-import V1HelloApi from './api/v1/hello';
-import V1UserApi from './api/v1/user';
-import V1AuthApi from './api/v1/auth';
-import V1RegisterApi from './api/v1/register';
-require('./database');
-import dotenv from 'dotenv';
-dotenv.config();
+import V1UserApi from './api/v1/user'
+import V1AuthApi from './api/v1/auth'
+import V1RegisterApi from './api/v1/register'
+require('./database')
+require('dotenv').config()
 
-const restify = require('restify');
+const restify = require('restify')
 
-const app = restify.createServer();
-app.use(restify.plugins.queryParser());
-app.use(restify.plugins.bodyParser());
+const app = restify.createServer()
+app.use(restify.plugins.queryParser())
+app.use(restify.plugins.bodyParser())
 
 const auth = new V1AuthApi([
   /api\/v1\/hello.*/,
   /api\/v1\/auth.*/,
   /api\/v1\/register.*/
-]);
-auth.apply(app);
+])
+auth.apply(app)
 
-const hello = new V1HelloApi();
-hello.apply(app);
+const user = new V1UserApi()
+user.apply(app)
 
-const user = new V1UserApi();
-user.apply(app);
+const register = new V1RegisterApi()
+register.apply(app)
 
-const register = new V1RegisterApi();
-register.apply(app);
+const port = process.env.API_PORT
 
-const port = process.env.API_PORT;
-
-app.listen(port, function() {
-  console.log('%s listening at %s', app.name, app.url);
-});
+app.listen(port, function () {
+  console.log('%s listening at %s', app.name, app.url)
+})
--- topix/database.js
+++ topix/database.js
@@ -1,28 +1,29 @@
-const mongoose = require('mongoose');
-mongoose.set('debug', true);
-require('dotenv').config();
+const mongoose = require('mongoose')
+mongoose.set('debug', true)
+require('dotenv').config()
 
-const host = process.env.MONGO_HOST;
-const database = process.env.MONGO_DATABASE;
-const username = process.env.MONGO_USERNAME;
-const password = process.env.MONGO_PASSWORD;
-const port = process.env.MONGO_PORT;
-const uri = `mongodb://${username}:${password}@${host}:${port}/${database}`;
+const host = process.env.MONGO_HOST
+const database = process.env.MONGO_DATABASE
+const username = process.env.MONGO_USERNAME
+const password = process.env.MONGO_PASSWORD
+const port = process.env.MONGO_PORT
+const uri = `mongodb://${username}:${password}@${host}:${port}/${database}`
 
 class Database {
-  constructor() {
-    this._connect();
+  constructor () {
+    this._connect()
   }
 
-  _connect() {
-     mongoose.connect(uri)
-       .then(() => {
-         console.log('Database connection successful')
-       })
-       .catch(err => {
-         console.error('Database connection error')
-       });
+  _connect () {
+    mongoose.connect(uri)
+      .then(() => {
+        console.log('Database connection successful')
+      })
+      .catch(err => {
+        console.error('Database connection error')
+        console.log(err)
+      })
   }
 }
 
-module.exports = new Database();
+module.exports = new Database()
