9d216be157dcc62ab2d0b8455ec10adf8fccb574
Jon Ball <jrball3@verizon.net>
2019-08-23 21:38:14 -0400

    simplify password logic

--- topix/utilities/passwords.js
+++ topix/utilities/passwords.js
@@ -2,26 +2,12 @@ const bcrypt = require('bcryptjs')
 const BCRYPT_SALT_ROUNDS = 12
 
 class Passwords {
-  hash (password) {
-    return new Promise(function (resolve, reject) {
-      try {
-        resolve(bcrypt.hashSync(password, BCRYPT_SALT_ROUNDS))
-      } catch (err) {
-        console.error(err)
-        reject(err)
-      }
-    })
+  async hash (password) {
+    return await bcrypt.hashSync(password, BCRYPT_SALT_ROUNDS)
   }
 
-  compare (pass1, pass2) {
-    return new Promise(function (resolve, reject) {
-      try {
-        resolve(bcrypt.compareSync(pass1, pass2))
-      } catch (err) {
-        console.error(err)
-        reject(err)
-      }
-    })
+  async compare (pass1, pass2) {
+    return await bcrypt.compareSync(pass1, pass2)
   }
 }
 
