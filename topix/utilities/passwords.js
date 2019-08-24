d595817e54d8a38d468cfac57852059a8ba2c279
Jon Ball <jrball3@verizon.net>
2019-08-23 21:39:21 -0400

    use async bcrypt

--- topix/utilities/passwords.js
+++ topix/utilities/passwords.js
@@ -3,11 +3,11 @@ const BCRYPT_SALT_ROUNDS = 12
 
 class Passwords {
   async hash (password) {
-    return await bcrypt.hashSync(password, BCRYPT_SALT_ROUNDS)
+    return await bcrypt.hash(password, BCRYPT_SALT_ROUNDS)
   }
 
   async compare (pass1, pass2) {
-    return await bcrypt.compareSync(pass1, pass2)
+    return await bcrypt.compare(pass1, pass2)
   }
 }
 
